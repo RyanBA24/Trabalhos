ff3a7fe8947fa0b8110d8254cc3fe03a4f060535
RyanBA24 <ryan2ba4@gmail.com>
2025-05-20 23:41:28 -0300

    Update Orçamento_domestico.py

--- Trabalhos_Capstone/Orçamento_domestico.py
+++ Trabalhos_Capstone/Orçamento_domestico.py
@@ -49,44 +49,6 @@ class OrcamentoDomestico:
 
         return total_receitas - total_despesas
     
-
-
-    def exportar_para_google_sheets(self):
-        # Define escopos
-        scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-        ]
-
-        creds = ServiceAccountCredentials.from_json_keyfile_name("caramel-era-460501-t3-140b194f4673.json", scope)
-        client = gspread.authorize(creds)
-        planilha = client.create("Orçamento Doméstico")
-        planilha.share('ryan2ba4@gmail.com', perm_type='user', role='writer')
-        # Adiciona abas e dados
-        aba_receitas = planilha.add_worksheet(title="Receitas", rows="100", cols="20")
-        aba_receitas.append_row(["Valor", "Data", "Descrição"])
-        for r in self.receitas:
-            aba_receitas.append_row([r["valor"], r["data"].strftime("%d-%m-%Y"), r["descricao"]])
-
-        aba_despesas = planilha.add_worksheet(title="Despesas", rows="100", cols="20")
-        aba_despesas.append_row(["Valor", "Data", "Descrição"])
-        for d in self.despesas:
-            aba_despesas.append_row([d["valor"], d["data"].strftime("%d-%m-%Y"), d["descricao"]])
-
-        aba_recorrentes = planilha.add_worksheet(title="Recorrentes", rows="100", cols="20")
-        aba_recorrentes.append_row(["Valor", "Tipo", "Início", "Fim", "Descrição"])
-        for r in self.recorrentes:
-            aba_recorrentes.append_row([
-                r["valor"], r["tipo"],
-                r["inicio"].strftime("%d-%m-%Y"),
-                r["fim"].strftime("%d-%m-%Y"),
-                r["descricao"]
-            ])
-
-        print("✅ Dados exportados com sucesso para o Google Sheets!")
-
-
-
     def menu(self):
         while True:
             print("1 - Adicionar Receita")
@@ -127,15 +89,9 @@ class OrcamentoDomestico:
                 print("Encerrando")
                 break
 
-            elif opcao == '6':
-                self.exportar_para_google_sheets()
-
             else:
                 print("Opção inválida, tente novamente")
                 continue
-
-
-
 
 
 # Execução principal
